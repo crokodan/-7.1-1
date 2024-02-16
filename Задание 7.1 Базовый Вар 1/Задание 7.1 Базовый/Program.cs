d9574c161de3eea3a62644bac127fbf400a304d2
Piholot2K (daniilhonda) <69081378+crokodan@users.noreply.github.com>
2024-02-16 19:13:25 +0400

    Add files via upload

---
+++ Задание 7.1 Базовый Вар 1/Задание 7.1 Базовый/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7._1_Базовый
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            const int n = 10 + 1;
+            char[] arr = new char[n];
+            string input = Console.ReadLine();
+            Array.Copy(input.ToCharArray(), arr, Math.Min(n, input.Length));
+
+            arr = arr.Select(c => char.IsDigit(c) ? '*' : c).ToArray();
+
+            Console.WriteLine(new string(arr));
+        }
+    }
+}
